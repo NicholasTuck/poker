715185c1fb659cf4d05d91d9e4e568f9b880ae2f
Nicholas Tuck <nick@nicholastuck.com>
2013-11-20 22:11:29 -0600

    Fix timer stopping. Add logging.

--- web/poker.dart
+++ web/poker.dart
@@ -2,14 +2,15 @@ import 'dart:html';
 import 'package:di/di.dart';
 import 'package:angular/angular.dart';
 import 'dart:async';
+import 'package:intl/intl.dart';
+import 'package:logging/logging.dart';
+import 'package:logging_handlers/logging_handlers_shared.dart';
 
 void main() {
   var module = new Module()
   ..type(PokerController);
 
   ngBootstrap(module:module);
-
-  new PokerController();
 }
 
 
@@ -19,12 +20,16 @@ void main() {
 )
 class PokerController {
   static const int NUMBER_OF_MINUTES = 1;  //TODO change back to 15
+  static final NumberFormat _formatter = new NumberFormat("00", "en_US");
+  static final Logger log = new Logger("PokerController");
   
   Stopwatch _timeElapsed = new Stopwatch();
-  int secondsRemaining = 00;
-  int minutesRemaining = NUMBER_OF_MINUTES;
+  int _secondsRemaining = 00;
+  int _minutesRemaining = NUMBER_OF_MINUTES;
   
   PokerController() {
+    Logger.root.level = Level.ALL;
+    startQuickLogging();
     _timeElapsed.start();
     
     
@@ -33,16 +38,24 @@ class PokerController {
   }
   
   void _updateTimeRemaining(Timer timer) {
-    minutesRemaining = NUMBER_OF_MINUTES - _timeElapsed.elapsed.inMinutes - 1;
-    secondsRemaining = 60 - _timeElapsed.elapsed.inSeconds % 60;
 
-    if (_timeElapsed.elapsed.compareTo(new Duration(minutes: NUMBER_OF_MINUTES)) >= 0) {
+    if (_timeElapsed.elapsed.compareTo(new Duration(minutes: NUMBER_OF_MINUTES)) < 0) {
+      
+      _minutesRemaining = NUMBER_OF_MINUTES - _timeElapsed.elapsed.inMinutes - 1;
+      _secondsRemaining = 60 - _timeElapsed.elapsed.inSeconds % 60;
+      
+      log.finest(_timeElapsed.elapsed.inSeconds.toString());
+    } else {
+      
       _timeElapsed.stop();
-      _timeElapsed.reset();
-      minutesRemaining = 0;
-      secondsRemaining = 0;
+      _minutesRemaining = 0;
+      _secondsRemaining = 0;
+      
     }
   }
+  
+  String get secondsRemaining => _formatter.format(_secondsRemaining);
+  String get minutesRemaining => _formatter.format(_minutesRemaining);
   
 }
 
