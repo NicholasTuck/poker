32b7b7587ac32a71c30d228b8cd6c93d523c0ff1
Nicholas Tuck <nick@nicholastuck.com>
2013-11-20 21:23:10 -0600

    Make timer work

--- web/poker.dart
+++ web/poker.dart
@@ -1,10 +1,7 @@
 import 'dart:html';
 import 'package:di/di.dart';
 import 'package:angular/angular.dart';
-
-import 'package:web_ui/web_ui.dart';
-
-
+import 'dart:async';
 
 void main() {
   var module = new Module()
@@ -21,27 +18,32 @@ void main() {
   publishAs: 'controller'
 )
 class PokerController {
-
-
-  Stopwatch stopwatch;
-  @observable  String timeElapsed = '00:00';
-  PokerController(){
-    stopwatch = new Stopwatch();
-
-      runInterval();
-
+  static const int NUMBER_OF_MINUTES = 1;  //TODO change back to 15
+  
+  Stopwatch _timeElapsed = new Stopwatch();
+  int secondsRemaining = 00;
+  int minutesRemaining = NUMBER_OF_MINUTES;
+  
+  PokerController() {
+    _timeElapsed.start();
+    
+    
+    new Timer.periodic(new Duration(seconds:1), _updateTimeRemaining);
+    
   }
+  
+  void _updateTimeRemaining(Timer timer) {
+    minutesRemaining = NUMBER_OF_MINUTES - _timeElapsed.elapsed.inMinutes - 1;
+    secondsRemaining = 60 - _timeElapsed.elapsed.inSeconds % 60;
 
-  void runInterval(){
-
-    stopwatch.start();
-    while (stopwatch.elapsedMilliseconds < 2000);
-    stopwatch.stop();
-    timeElapsed = stopwatch.elapsedMilliseconds.toString();
-
-
+    if (_timeElapsed.elapsed.compareTo(new Duration(minutes: NUMBER_OF_MINUTES)) >= 0) {
+      _timeElapsed.stop();
+      _timeElapsed.reset();
+      minutesRemaining = 0;
+      secondsRemaining = 0;
+    }
   }
-
+  
 }
 
 
