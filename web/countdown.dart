66b08e0ac13d58ec30adc690144546bc93e60283
Brandon McAllister <brandon@insomniacisland.com>
2013-11-23 03:33:45 -0600

    cleaned up countdown - now using duration not minutes and seconds

--- web/countdown.dart
+++ web/countdown.dart
@@ -15,26 +15,24 @@ import 'dart:core';
     })
 class CountdownController {
   static const DEBUGGING = false;
-  
-  static final NumberFormat _formatter = new NumberFormat("00", "en_US");
   static final Logger log = new Logger("PokerController");
+
+  static final DateFormat _formatter = new DateFormat.ms();
+  static final DateTime TIME_ZERO = new DateTime.fromMillisecondsSinceEpoch(0);
   
   static const String NORMAL_COLOR_CLASS = "normal";
   static const String WARNING_COLOR_CLASS = "warning";
-  static const double WARNING_PERCENTAGE = .7;
+  static const double WARNING_PERCENTAGE = 0.7;
   static const String DANGER_COLOR_CLASS = "danger";
   
   Scope _scope;
   String colorClass;
   
   Stopwatch _stopWatch = new Stopwatch();
-  int _startTime;
-  int _secondsRemaining;
-  int _minutesRemaining;
-  
+  Duration _startingDuration;
+  Duration _timeRemaining;
   
   CountdownController(Scope this._scope) {
-    _minutesRemaining = _startTime;
     new Timer.periodic(new Duration(seconds:1), _updateTimeRemaining);
     
     _scope.$on("toggleTimer", toggleTimer);
@@ -46,14 +44,13 @@ class CountdownController {
   }
   
   void initiliazeCountdown(int startTime) {
-    _startTime = startTime;
-    _minutesRemaining = _startTime;
-    _secondsRemaining = 0;
+    _startingDuration = new Duration(minutes:startTime);
+    _timeRemaining = new Duration(minutes:startTime);
     colorClass = NORMAL_COLOR_CLASS;
   }
   
-  String get timeRemaining => "${_formatter.format(_minutesRemaining)}:${_formatter.format(_secondsRemaining)}";
-
+  String get timeRemainingText => _formatter.format(TIME_ZERO.add(_timeRemaining));
+  
   void toggleTimer() {
     _stopWatch.isRunning ? _stopWatch.stop() : _stopWatch.start();
     _scope.$emit("timerToggled", new List<bool>()..add(_stopWatch.isRunning));
@@ -62,37 +59,37 @@ class CountdownController {
   void resetTimer() {
     if(_stopWatch.isRunning) toggleTimer();
     _stopWatch.reset();
-    initiliazeCountdown(_startTime);
+    initiliazeCountdown(_startingDuration.inMinutes);
     _scope.$emit("timerReset");
   }
   
   void _updateTimeRemaining([Timer timer = null]) {
-
     if (_stopWatch.isRunning) { 
-      if (_stopWatch.elapsed.compareTo(new Duration(minutes: _startTime)) < 0 
-          && !(DEBUGGING && _stopWatch.elapsed.compareTo(new Duration(seconds: 5)) > 0)) {    // make rounds only 5 seconds while debugging
-        
-        _minutesRemaining = _startTime - _stopWatch.elapsed.inMinutes  - 1;
-        _secondsRemaining = 60 - _stopWatch.elapsed.inSeconds % 60 - 1;
-        
-        if (_stopWatch.elapsed.compareTo(new Duration(minutes:_startTime - 1)) > 0) {
-          colorClass = DANGER_COLOR_CLASS;
-        } else if (_stopWatch.elapsed.compareTo(new Duration(minutes: (_startTime * WARNING_PERCENTAGE).round())) > 0) {
-          colorClass = (DEBUGGING ? .1 : WARNING_COLOR_CLASS);
-        } 
-        
-      } else {
+      bool isTimeExpired = _timeRemaining.inSeconds <= 0;
+      bool isExpiredForDebugging = DEBUGGING && _stopWatch.elapsed.inSeconds > 5; // make rounds only 5 seconds while debugging
+      
+      if (isTimeExpired || isExpiredForDebugging) {
         countdownComplete();
+      } else {
+        _timeRemaining = _startingDuration - _stopWatch.elapsed;
+        _updateColorClass();
       }
+    }
+  }
+
+  void _updateColorClass() {
+    int warningMinutes = (_startingDuration.inMinutes * WARNING_PERCENTAGE).round();
+    
+    if (_timeRemaining.inMinutes <= 1) {
+      colorClass = DANGER_COLOR_CLASS;
+    } else if (_timeRemaining.inMinutes <= warningMinutes) {
+      colorClass = (DEBUGGING ? .1 : WARNING_COLOR_CLASS);
     }
   }
 
   void countdownComplete() {
     toggleTimer();
-    _minutesRemaining = 0;
-    _secondsRemaining = 0;
+    _timeRemaining = Duration.ZERO;
     _scope.$emit("countdownComplete");
   }
-  
-  
 }
