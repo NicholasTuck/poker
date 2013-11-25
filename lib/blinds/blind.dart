646a367b700a826483d11f12d7b4bb9278589d4a
Nicholas Tuck <nick@nicholastuck.com>
2013-11-24 21:21:20 -0600

    add Blind class.  Refactor blinds to use Blind objects

---
+++ lib/blinds/blind.dart
@@ -0,0 +1,14 @@
+library blinds;
+
+class Blind {
+  int smallBlind;
+  int bigBlind;
+  int ante;
+  
+  Blind.blindsOnly(this.smallBlind, this.bigBlind): ante = 0;
+
+  Blind.anteOnly(this.ante): smallBlind = 0, bigBlind = 0;
+
+  Blind(this.smallBlind, this.bigBlind): ante = 0;
+  
+}
